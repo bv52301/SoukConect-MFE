f7990ae00807d34181d17ca687aabae23477ce7c
balajij81 <balaji.jayaraman81@gmail.com>
2026-02-26 00:36:54 +0800

    Add basePath to Next.js configuration

--- apps/food-connect/next.config.ts
+++ apps/food-connect/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: require("path").join(__dirname, "../../"),
+  basePath: '/food',
 };
 
 export default nextConfig;
